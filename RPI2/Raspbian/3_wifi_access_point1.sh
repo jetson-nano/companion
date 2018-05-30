f3696a858120d3ac18275f365294ba20f1fdce8d
Jaime Machuca <jaime@element.aero>
2018-05-29 20:12:16 -0500

    removed files that are now on the pywificontrol repo and added install script file

--- RPI2/Raspbian/3_wifi_access_point1.sh
+++ RPI2/Raspbian/3_wifi_access_point1.sh
@@ -17,8 +17,55 @@ pip install eventlet flask flask_socketio redis
 
 apt-get install -y python-dbus python-netifaces python-pexpect wpasupplicant hostapd wireless-tools udhcpd udhcpc
 apt-get install -y dbus libdbus-glib-1-dev libdbus-1-dev python-dbus redis-tools
+apt-get install avahi-daemon
 
-sudo apt-get install avahi-daemon
+pushd GitHub/
+git clone https://github.com/jmachuca77/pywificontrol.git
+git clone https://github.com/emlid/systemd-manager.git
 
-pushd GitHub/companion/RPI2/Raspbian/WiFiControl
-cp
+pushd systemd-manager
+python setup.py build install
+popd
+
+pushd pywificontrol
+git checkout Ardupilot_v0.4.0
+python setup.py build install
+
+pushd tools/wpa_supplicant
+cp * /etc/wpa_supplicant
+popd
+
+pushd tools/hostapd
+cp * /etc/hostapd
+popd
+
+chmod +x /etc/wpa_supplicant/*.sh
+chmod +x /etc/hostapd/*.sh
+
+pushd tools
+cp *.service /lib/systemd/system
+
+pushd server
+cp -r * /home/apsync/start_wificontrol/
+
+
+sudo -u $NORMAL_USER -H bash <<'EOF'
+set -e
+set -x
+
+# auto start wificontrol and server
+WIFICONTROL_HOME=~/start_wificontrol
+if [ ! -d $WIFICONTROL_HOME ]; then
+    mkdir $WIFICONTROL_HOME
+fi
+cp startup_wifiserver.sh $WIFICONTROL_HOME/
+cp init_wifi.py $WIFICONTROL_HOME/
+
+chmod +x $WIFICONTROL_HOME/init_wifi.py
+chmod +x $WIFICONTROL_HOME/startup_WifiServer.sh
+
+LINE="# sudo -H -u $NORMAL_USER /bin/bash -c '~$NORMAL_USER/start_wificontrol/startup_WifiServer.sh'"
+perl -pe "s%^exit 0%$LINE\\n\\nexit 0%" -i /etc/rc.local
+
+systemctl disable networking
+systemctl disable dhcpcd
