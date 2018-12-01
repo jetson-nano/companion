75139fd1d9db7c5021d0afefb1a26d814edde3ac
jmachuca77 <jaime@element.aero>
2018-12-01 15:13:47 -0700

    Made substantial changes and updates

--- Nvidia_JTX1/Ubuntu/Google_Cartographer/Install_google_cartographer.sh
+++ Nvidia_JTX1/Ubuntu/Google_Cartographer/Install_google_cartographer.sh
@@ -15,23 +15,39 @@ apt-get install python-wstool python-rosdep ninja-build
 
 pushd $HOME
 mkdir -p GoogleCartographer_ws/src
+mkdir -p GoogleCartographer_ws/launch
+
+pushd $HOME/GoogleCartographer_ws
+catkin_init_workspace
 
 pushd $HOME/GoogleCartographer_ws/src
 git clone https://github.com/Slamtec/rplidar_ros.git
-
+git clone https://github.com/GT-RAIL/robot_pose_publisher.git
+popd
 
 
 wstool init src
 wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
 wstool update -t src
 
-pushd cartographer/scripts/
+pushd $HOME/GoogleCartographer_ws/src/cartographer/scripts
 ./install_proto3.sh
+popd
 
 sudo rosdep init   # if error message appears about file already existing, just ignore and continue
 rosdep update
 rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
 
+pushd $HOME/GoogleCartographer_ws/src/robot_pose_publisher/src
+sed -i 's/"is_stamped", is_stamped, false"/""is_stamped", is_stamped, true"/g' robot_pose_publisher.cpp
 popd
-git clone https://github.com/GT-RAIL/robot_pose_publisher.git
+popd
+popd
 
+cp robot_pose_publisher.cpp $HOME/GoogleCartographer_ws/src/robot_pose_publisher/src
+cp cartographer.launch $HOME/GoogleCartographer_ws/src/cartographer_ros/cartographer_ros/launch
+cp cartographer.lua $HOME/GoogleCartographer_ws/src/cartographer_ros/cartographer_ros/configuration_files
+
+cp ArdupilotMavROS.launch $HOME/GoogleCartographer_ws/launch
+
+echo "Successfully installed Google Cartographer and Ardupilot Project"
